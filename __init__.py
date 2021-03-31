ae85e5e2388994e1f984e7fd9cbea008aae4f63d
Hadrien <ketsapiwiq@protonmail.com>
2021-03-31 15:20:27 +0200

    first good version

--- __init__.py
+++ __init__.py
@@ -1,108 +1,131 @@
 import bme680
-# import display, utime, vibra, os, bme680, color, buttons, leds
-
-# led_bar_colours = [[255, 0, 255], [127, 0, 255], [0, 0, 255], [0, 128, 255], [0, 255, 255], [0, 255, 128], [0, 255, 0], [128, 255, 0], [255, 255, 0], [255, 128, 0], [255, 0, 0] ]
-
-# bme680.init()
-disp = display.open()
-
-leds.clear()
-
-for x in range(0, 11):
-    leds.prep(10-x, led_bar_colours[x])
-
-leds.update()
-
-vibra.vibrate(50)
-utime.sleep(0.25)
-vibra.vibrate(50)
-linecolor = color.Color(100, 100, 100) # grey
-rectangleColor = color.Color(0, 255, 0)
-arrowPosition = 0
-show_led_bar = True
+import leds
+import time
+import display
+# import color
 
 
-def print_arrow(peak_x, peak_y):
-    disp.line(xs=peak_x, ys=peak_y, xe=peak_x+4, ye= peak_y-4, col=rectangleColor)
-    disp.line(xs=peak_x, ys=peak_y, xe=peak_x+5, ye= peak_y+5, col=rectangleColor)
+colors = {'lime': [0, 255, 0], 'green': [0, 128, 0], 'yellow': [255, 255, 0], 'orange': [
+    255, 128, 0], 'red': [255, 0, 0], 'purple': [128, 0, 128], 'brown': [128, 0, 0]}
 
-def check_arrow(arrow_position):
-    if arrow_position == 0:
-        return 150, 7
-    if arrow_position == 1:
-        return 150, 27
-    if arrow_position == 2:
-        return 150, 47
-    if arrow_position == 3:
-        return 150, 67
 
-def led_bar(value, min_v, max_v):
-    value = value - min_v
-    max_v = max_v - min_v
-    steps = max_v / 11.0
-    current_step = value / steps
-    current_step = int(current_step + 0.5)
+gs = 160
+all_colors = [((i >> 2) * gs, (i >> 1 & 1) * gs, (i & 1) * gs) for i in range(1, 8)]
+
+_rand = 123456789
+def rand():
+    global _rand
+    _rand = (1103515245 * _rand + 12345) & 0xFFFFFF
+    return _rand
+
+
+def iaq_color(iaq):
+    if (iaq < 50):
+        return colors['lime']
+    if (iaq < 100):
+        return colors['green']
+    if (iaq < 150):
+        return colors['yellow']
+    if (iaq < 200):
+        return colors['orange']
+    if (iaq < 250):
+        return colors['red']
+    if (iaq <= 350):
+        return colors['purple']
+    if (iaq > 350):
+        return colors['brown']
+
+def iaq_string(iaq):
+    if (iaq < 50):
+        return "Excellent"
+    if (iaq < 100):
+        return "Good"
+    if (iaq < 150):
+        return "OK"
+    if (iaq < 200):
+        return "Ventilate?"
+    if (iaq < 250):
+        return "Ventilate!"
+    if (iaq <= 350):
+        return "Dangerous"
+    if (iaq > 350):
+        return "Leave!"
+
+
+def main():
+
+    disp = display.open()
+    disp.clear().update()
     leds.clear()
-    for x in range(0, current_step):
-        leds.prep(10-x, led_bar_colours[x]) # 10 - 3 to reverse LEDs
+    # leds.set_powersave()
 
-    leds.update()
+    with bme680.Bme680() as environment:
+        while True:
+            data = environment.get_data()
 
+            disp.clear()
 
-def check_led_bar(arrow_position):
-    if arrow_position == 0:
-        return 0, 40
-    if arrow_position == 1:
-        return 0, 100
-    if arrow_position == 2:
-        return 325, 1070
-    if arrow_position == 3:
-        return 8000, 550000
+            if(data.iaq_accuracy >= 2):
+                disp.print("IAQ: " + str(data.iaq))
+                disp.print(iaq_string(data.iaq), posy=20)
+                disp.print("CO2: " +
+                       str(round(data.eco2, 1)), posy=40)
+                
+                if(data.iaq_accuracy == 2):
+                    disp.print("calibrating...", posy=60)
 
+                leds.set_all([iaq_color(data.iaq)]*15)
+                time.sleep(10)
 
-while True:
-    env = bme680.get_data()
-    #brightness = light_sensor.get_reading()
-    disp.clear()
-    disp.print(str(round(env[0], 1)) + " C")
-    disp.line(xs=1, ys=17, xe=159, ye= 17, col=linecolor)
-    disp.print(str(round(env[1], 1)) + " %", posy=20)
-    disp.line(xs=1, ys=37, xe=159, ye= 37, col=linecolor)
-    disp.print(str(round(env[2], 1)) + " hPa", posy=40)
-    disp.line(xs=1, ys=57, xe=159, ye= 57, col=linecolor)
-    disp.print(str(int(env[3])) + " Ohm", posy=60)
-    #*****
-    #disp.print(str(brightness), posx=105)
-
-    #disp.rect(xs=150, ys=10, xe=158, ye=80, col=rectangleColor, filled=True, size=5)
-    valueX, valueY = check_arrow(arrowPosition)
-    print_arrow(valueX, valueY)
-
-    if show_led_bar:
-        min_led, max_led = check_led_bar(arrowPosition)
-        led_bar(env[arrowPosition], min_led, max_led)
-    else:
-        leds.clear()
-        leds.set_powersave(True) 
-
-    disp.update()
-    for x in range(0,20):
-        utime.sleep(0.05)
-
-        if buttons.read(buttons.BOTTOM_LEFT):
-            arrowPosition -= 1
-            arrowPosition = arrowPosition % 4
-            break
-
-        if buttons.read(buttons.BOTTOM_RIGHT):
-            arrowPosition += 1
-            arrowPosition = arrowPosition % 4
-            break
-
-        if buttons.read(buttons.TOP_RIGHT):
-            if show_led_bar:
-                show_led_bar = False
-                break
             else:
-                show_led_bar = True
-                break
+                disp.print("calibrating...", posy=20)
+                disp.update()
+                oldest_time = time.time()
+                latest_time = oldest_time
+                while (latest_time - oldest_time < 10):
+                    leds.set(rand() % 11, all_colors[rand() % len(all_colors)])
+                    leds.set_rocket(rand() % 3, rand() % 32)
+                    time.sleep_us(1)  # Feed watch doge
+                    latest_time = time.time()
+
+            disp.update()
+
+
+if __name__ == "__main__":
+    main()
+
+    # # *****
+    # # disp.print(str(brightness), posx=105)
+
+    # # disp.rect(xs=150, ys=10, xe=158, ye=80, col=rectangleColor, filled=True, size=5)
+    # valueX, valueY = check_arrow(arrowPosition)
+    # print_arrow(valueX, valueY)
+
+    # if show_led_bar:
+    #     min_led, max_led = check_led_bar(arrowPosition)
+    #     led_bar(env[arrowPosition], min_led, max_led)
+    # else:
+    #     leds.clear()
+    #     leds.set_powersave(True)
+
+    # disp.update()
+    # for x in range(0,20):
+    #     utime.sleep(0.05)
+
+    #     if buttons.read(buttons.BOTTOM_LEFT):
+    #         arrowPosition -= 1
+    #         arrowPosition = arrowPosition % 4
+    #         break
+
+    #     if buttons.read(buttons.BOTTOM_RIGHT):
+    #         arrowPosition += 1
+    #         arrowPosition = arrowPosition % 4
+    #         break
+
+    #     if buttons.read(buttons.TOP_RIGHT):
+    #         if show_led_bar:
+    #             show_led_bar = False
+    #             break
+    #         else:
+    #             show_led_bar = True
+    #             break
